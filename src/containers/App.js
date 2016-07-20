c03e96770aac7bec142d54721b5d05967f6ca5e3
Tony Ryzhikov <selfdeceited@gmail.com>
2016-07-20 20:36:02 +0300

    let's use es6 on node as well!  but without import :(

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,6 @@ import React, { Component, PropTypes } from 'react'
 
 export default class App extends Component {
   render() {
-    return <div>hello from the other side</div>
+    return <div>no way!</div>
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,4 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from './containers/App'
 
-render(
-  <App />,
-  document.getElementById('root')
-)
+render(<App/>, document.getElementById('root'))
